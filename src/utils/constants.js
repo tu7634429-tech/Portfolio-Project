a7cab983eeb25c808ea498d228f9b6d956342bcf
tu7634429-tech <tu7634429@gmail.com>
2026-04-23 12:24:24 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const SITE_CONFIG = {
   name: "Thevindu Ransara",
-  role: "Software Engineer",
-  email: "Ransara@example.com",
-  phone: "+94 77 000 0000",
+  role: "UI/Ux Engineer",
+  email: "ransararathnayaka85@gmail.com",
+  phone: "+94 78 297 6516",
   location: "Sri Lanka",
 };
